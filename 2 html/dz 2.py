1778ad5f85987d0882d53f79ab09401f5d3cda77
Anton Romashkin <anton.v.romashkin@gmail.com>
2020-07-30 17:11:12 +0300

    superjob готов

--- 2 html/dz 2.py
+++ 2 html/dz 2.py
@@ -2,26 +2,35 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 import pandas as pd
-import pprint
 
-search = 'Data science'
+search = input('Поиск вакансии: ')
 
 header = {
-    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'}
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'}
 
 # тянем с SuperJob
-
 superjob_link = 'https://www.superjob.ru'
 page = 1
-params = {
-    'keywords':'Управляющий салоном',
-    'noGeo':'1',
-    'page':page
+
+vacs = {
+    "name": [],
+    "link": [],
+    "location": [],
+    "employer": [],
+    "source": [],
+    "min_salary": [],
+    "max_salary": [],
+    "currency": [],
 }
 
-vacancies = []
+vacancies = pd.DataFrame(vacs)
 
 while True:
+    params = {
+        'keywords': search,
+        'noGeo': '1',
+        'page': page
+    }
     sj_html = requests.get(superjob_link + '/vacancy/search/', params=params)
     soup = bs(sj_html.text, 'lxml')
 
@@ -30,39 +39,59 @@ while True:
         vacancy_data = {}
         min_salary = None
         max_salary = None
+        currency = None
         vac_name = vac.find('a', {'class': '_6AfZ9'}).getText()
         vac_link = vac.find('a', {'class': '_6AfZ9'})['href']
-        employer = vac.find('a', {'class': '_25-u7'}).getText()
-        location = vac.find('span', {'class': 'f-test-text-company-item-location'}).findChildren()[-1].getText()
+
+        if vac.find('a', {'class': '_25-u7'}) != None:
+            employer = vac.find('a', {'class': '_25-u7'}).getText()
+        else:
+            employer = None
+
+        if vac.find('span', {'class': 'f-test-text-company-item-location'}).findChildren()[-1].getText() != None:
+            location = vac.find('span', {'class': 'f-test-text-company-item-location'}).findChildren()[-1].getText()
+        else:
+            location = None
+
         source = 'superjob.ru'
+
         raw_salary = vac.find('span', {'class': '_2VHxz'}).getText()
-        if 'до' in raw_salary:
-            max_salary = ''.join(re.findall(r'\d', raw_salary))
-        elif 'от' in raw_salary:
-            min_salary = ''.join(re.findall(r'\d', raw_salary))
-        elif '—' in raw_salary:
-            salary_range = re.split(r'—', ''.join(re.findall(r'\d|—', raw_salary)))
-            min_salary = salary_range[0]
-            max_salary = salary_range[1]
+        if re.findall(r'\S\w+', raw_salary)[-1] != 'договорённости':
+            if 'до' in raw_salary:
+                max_salary = ''.join(re.findall(r'\d', raw_salary))
+                currency = re.findall(r'\S\w+', raw_salary)[-1]
+            elif 'от' in raw_salary:
+                min_salary = ''.join(re.findall(r'\d', raw_salary))
+                currency = re.findall(r'\S\w+', raw_salary)[-1]
+            elif '—' in raw_salary:
+                salary_range = re.split(r'—', ''.join(re.findall(r'\d|—', raw_salary)))
+                min_salary = salary_range[0]
+                max_salary = salary_range[1]
+                currency = re.findall(r'\S\w+', raw_salary)[-1]
 
-        vacancy_data['name'] = vac_name
-        vacancy_data['link'] = vac_link
-        vacancy_data['location'] = location
-        vacancy_data['employer'] = employer
-        vacancy_data['source'] = source
-        vacancy_data['min_salary'] = min_salary
-        vacancy_data['max_salary'] = max_salary
+        vacancy_data = pd.DataFrame({'name': [vac_name],
+                                     'link': [vac_link],
+                                     'location': [location],
+                                     'employer': [employer],
+                                     'source': [source],
+                                     'min_salary': [min_salary],
+                                     'max_salary': [max_salary],
+                                     'currency': [currency]
+                                     })
 
+        vacancies = pd.concat([vacancies, vacancy_data], axis=0)
 
-
-
-    page_check = soup.find('div', {'class': '_GJem'}).getText()
-    if 'Дальше' in page_check:
-        page += 1
-    else:
+    if soup.find('div', {'class': '_GJem'}) == None:
         break
+    else:
+        page_check = soup.find('div', {'class': '_GJem'}).getText()
+        if 'Дальше' in page_check:
+            page += 1
+        else:
+            break
 
-
+new_file = 'result.csv'
+vacancies.to_csv(new_file, index=False, encoding='utf-8')
 
 """
 page = 0
