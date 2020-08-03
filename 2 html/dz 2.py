9c93808ab079cf59e7188fb9d68d69f64f8596a4
Anton Romashkin <anton.v.romashkin@gmail.com>
2020-08-03 13:22:06 +0300

    выполнил задание 2

--- 2 html/dz 2.py
+++ 2 html/dz 2.py
@@ -2,14 +2,127 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 import pandas as pd
+import time
+
+
+def format_salary(raw_salary):
+    """
+    Функция для преобразования строки, содержащей зарплатную вилку. Возвращает min_salary, max_salary, currency
+    """
+    min_salary = None
+    max_salary = None
+    currency = None
+    if 'до' in raw_salary:
+        max_salary = ''.join(re.findall(r'\d', raw_salary))
+        currency = re.findall(r'\S\w+', raw_salary)[-1]
+    elif 'от' in raw_salary:
+        min_salary = ''.join(re.findall(r'\d', raw_salary))
+        currency = re.findall(r'\S\w+', raw_salary)[-1]
+    elif '-' in raw_salary:
+        salary_range = re.split(r'-', ''.join(re.findall(r'\d|-', raw_salary)))
+        min_salary = salary_range[0]
+        max_salary = salary_range[1]
+        currency = re.findall(r'\S\w+', raw_salary)[-1]
+    return min_salary, max_salary, currency
+
+
+header = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+}
 
 search = input('Поиск вакансии: ')
 
-header = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'}
+# создаём пустой dataframe
+vacs = {
+    "name": [],
+    "link": [],
+    "location": [],
+    "employer": [],
+    "source": [],
+    "min_salary": [],
+    "max_salary": [],
+    "currency": [],
+}
+hh_vacancies = pd.DataFrame(vacs)
+
+# тянем с hh
+
+hh_link = 'https://hh.ru'
+hh_addlink = '/search/vacancy'
+page = 0
+
+# создаём цикл для смены страниц сайта
+while True:
+    params = {
+        'L_save_area': 'true',
+        'clusters': 'true',
+        'showClusters': 'true',
+        'search_field': 'name',
+        'enable_snippets': 'true',
+        'text': search,
+        'page': page
+    }
+
+    hh_html = requests.get(f'{hh_link + hh_addlink}', params, headers=header)
+    hh_soup = bs(hh_html.text, 'lxml')
+
+    vac_list = hh_soup.findAll('div', {'class': 'vacancy-serp-item'})
+    # цикл для перебора вакансий внутри одной страницы
+    for vac in vac_list:
+        vac_data = {}
+        # name
+        vac_name = vac.find('a').getText()
+        # link
+        vac_link = vac.find('a')['href']  # link
+        # employer
+        if vac.find('a', {'class': 'bloko-link bloko-link_secondary'}).getText() is not None:
+            employer = vac.find('a', {'class': 'bloko-link bloko-link_secondary'}).getText()
+        else:
+            employer = None
+        # location
+        if vac.find('span', {'data-qa': 'vacancy-serp__vacancy-address'}).getText() is not None:
+            location = vac.find('span', {'data-qa': 'vacancy-serp__vacancy-address'}).getText().split(',')[0]
+        else:
+            location = None
+        # source
+        source = 'hh.ru'
+        # salary
+        raw_salary = []
+        if len(vac.find('div', {'class': 'vacancy-serp-item__sidebar'}).findChildren()) > 0:
+            raw_salary = vac.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}).getText()
+
+        min_salary, max_salary, currency = format_salary(raw_salary)
+
+        # dataframe
+        # заполняем таблицу для вакансии
+        vacancy_data = pd.DataFrame({
+            'name': [vac_name],
+            'link': [vac_link],
+            'location': [location],
+            'employer': [employer],
+            'source': [source],
+            'min_salary': [min_salary],
+            'max_salary': [max_salary],
+            'currency': [currency]
+        })
+        # собираем данные по всем вакансиям в одну таблицу
+        hh_vacancies = pd.concat([hh_vacancies, vacancy_data], axis=0)
+    # проверка для смены страциц - ищем кнопку 'дальше'
+    if hh_soup.find('a', {'class': 'HH-Pager-Controls-Next'}) is None:
+        break
+    else:
+        page_check = hh_soup.find('a', {'class': 'HH-Pager-Controls-Next'}).getText()
+        if 'дальше' in page_check:
+            page += 1
+        else:
+            break
+    # ждём 1 секунду перед сменой страницы сайта
+    time.sleep(1)
 
 # тянем с SuperJob
-superjob_link = 'https://www.superjob.ru'
+
+sj_link = 'https://www.superjob.ru'
+sj_addlink = '/vacancy/search/'
 page = 1
 
 vacs = {
@@ -23,7 +136,7 @@ vacs = {
     "currency": [],
 }
 
-vacancies = pd.DataFrame(vacs)
+sj_vacancies = pd.DataFrame(vacs)
 
 while True:
     params = {
@@ -31,44 +144,37 @@ while True:
         'noGeo': '1',
         'page': page
     }
-    sj_html = requests.get(superjob_link + '/vacancy/search/', params=params)
-    soup = bs(sj_html.text, 'lxml')
 
-    vac_list = soup.findAll('div', {'class': 'f-test-vacancy-item'})
+    sj_html = requests.get(f'{sj_link + sj_addlink}', params=params)
+    sj_soup = bs(sj_html.text, 'lxml')
+
+    vac_list = sj_soup.findAll('div', {'class': 'f-test-vacancy-item'})
     for vac in vac_list:
-        vacancy_data = {}
-        min_salary = None
-        max_salary = None
-        currency = None
+        vac_data = {}
+        # name
         vac_name = vac.find('a', {'class': '_6AfZ9'}).getText()
+        # link
         vac_link = vac.find('a', {'class': '_6AfZ9'})['href']
-
-        if vac.find('a', {'class': '_25-u7'}) != None:
+        # employer
+        if vac.find('a', {'class': '_25-u7'}) is not None:
             employer = vac.find('a', {'class': '_25-u7'}).getText()
         else:
             employer = None
-
-        if vac.find('span', {'class': 'f-test-text-company-item-location'}).findChildren()[-1].getText() != None:
-            location = vac.find('span', {'class': 'f-test-text-company-item-location'}).findChildren()[-1].getText()
+        # location
+        if vac.find('span', {'class': 'f-test-text-company-item-location'}).findChildren()[-1].getText() is not None:
+            location = vac.find('span', {'class': 'f-test-text-company-item-location'}).findChildren()[2].getText().split(',')[0]
         else:
             location = None
-
+        # source
         source = 'superjob.ru'
+        # salary
+        raw_salary = []
+        if len(vac.find('span', {'class': '_2VHxz'})) > 0 and re.findall(r'\S\w+', vac.find('span', {'class': '_2VHxz'}).getText())[-1] != 'договорённости':
+            raw_salary = vac.find('span', {'class': '_2VHxz'}).getText()
 
-        raw_salary = vac.find('span', {'class': '_2VHxz'}).getText()
-        if re.findall(r'\S\w+', raw_salary)[-1] != 'договорённости':
-            if 'до' in raw_salary:
-                max_salary = ''.join(re.findall(r'\d', raw_salary))
-                currency = re.findall(r'\S\w+', raw_salary)[-1]
-            elif 'от' in raw_salary:
-                min_salary = ''.join(re.findall(r'\d', raw_salary))
-                currency = re.findall(r'\S\w+', raw_salary)[-1]
-            elif '—' in raw_salary:
-                salary_range = re.split(r'—', ''.join(re.findall(r'\d|—', raw_salary)))
-                min_salary = salary_range[0]
-                max_salary = salary_range[1]
-                currency = re.findall(r'\S\w+', raw_salary)[-1]
+        min_salary, max_salary, currency = format_salary(raw_salary)
 
+        # заполняем таблицу для вакансии
         vacancy_data = pd.DataFrame({'name': [vac_name],
                                      'link': [vac_link],
                                      'location': [location],
@@ -78,47 +184,23 @@ while True:
                                      'max_salary': [max_salary],
                                      'currency': [currency]
                                      })
-
-        vacancies = pd.concat([vacancies, vacancy_data], axis=0)
-
-    if soup.find('div', {'class': '_GJem'}) == None:
+        # объединяем данные в одну таблицу
+        sj_vacancies = pd.concat([sj_vacancies, vacancy_data], axis=0)
+    # проверка страниц, ищем кнопку 'Дальше'
+    if sj_soup.find('div', {'class': '_GJem'}) is None:
         break
     else:
-        page_check = soup.find('div', {'class': '_GJem'}).getText()
+        page_check = sj_soup.find('div', {'class': '_GJem'}).getText()
         if 'Дальше' in page_check:
             page += 1
         else:
             break
+    # ждём секунду перед сменой страницы
+    time.sleep(1)
+
+# объединяем данные с разных сайтов
+vacancies = pd.concat([hh_vacancies, sj_vacancies], axis=0)
 
 new_file = 'result.csv'
 vacancies.to_csv(new_file, index=False, encoding='utf-8')
 
-"""
-page = 0
-main_link = 'https://hh.ru'
-params = {
-    'L_is_autosearch':'false',
-    'clusters':'true',
-    'enable_snippets':'true',
-    'text':'Data science',
-    'page':'0'
-}
-
-
-html = requests.get('https://www.superjob.ru')
-soup = bs(html.text, 'lxml')
-
-vac_block = soup.find('div', {'class':'vacancy-serp'})
-vac_list = vac_block.findAll('div', {'class': 'vacancy-serp__vacancy'})
-
-vacs = []
-
-for vac in vac_list:
-    vac_data = {}
-    vac_name = vac.find('div', {'class': 'vacancy-serp-item__row_header'}).children[-1].getText()
-    vac_employer = vac
-print(0)
-# https://hh.ru/search/vacancy?L_is_autosearch=false&clusters=true&enable_snippets=true&text=Data+science&page=1
-bs.g
-
-"""
